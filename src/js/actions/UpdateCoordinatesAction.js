c0f0e3a84b0a6c02a3c79ea5725b636aefda84ae
AAR1069 <scottaaron@northwesternmutual.com>
2018-05-15 14:04:10 -0500

    add a catch handler in the request process to use the internal calculation as a fallback

--- src/js/actions/UpdateCoordinatesAction.js
+++ src/js/actions/UpdateCoordinatesAction.js
@@ -21,38 +21,41 @@ function convertCoordinate(coordinate) {
     };
 }
 
-function calculateDistance1(firstCoordinate, secondCoordinate) {    
-/*    // do the calculation myself according to https://en.wikipedia.org/wiki/Great-circle_distance
-    var firstLatitude = toRadians(firstCoordinate.lat);
-    var firstLongitude = toRadians(firstCoordinate.lng);
-    var secondLatitude = toRadians(secondCoordinate.lat);
-    var secondLongitude = toRadians(secondCoordinate.lng);
+function calculateDistanceInternal(firstCoordinate, secondCoordinate) {
+    // do the calculation myself according to https://en.wikipedia.org/wiki/Great-circle_distance
+     var firstLatitude = toRadians(firstCoordinate.lat);
+     var firstLongitude = toRadians(firstCoordinate.lng);
+     var secondLatitude = toRadians(secondCoordinate.lat);
+     var secondLongitude = toRadians(secondCoordinate.lng);
+     
+     var delLatitude = secondLatitude - firstLatitude;
+     var delLongitude = secondLongitude - firstLongitude;
+     
+     var term1 = Math.sin(delLatitude / 2);
+     var term2 = Math.sin(delLongitude / 2);
+     
+     var delSigma = 2 * Math.asin(Math.sqrt(Math.pow(term1, 2) + Math.cos(firstLatitude) * Math.cos(secondLatitude) * Math.pow(term2, 2)));
+     var myDistance = EARTH_RADIUS * delSigma;
+     
+     console.log("my calculation: " + myDistance);
+     
+     // use my value for now
+     return myDistance;   
+}
 
-    var delLatitude = secondLatitude - firstLatitude;
-    var delLongitude = secondLongitude - firstLongitude;
-
-    var term1 = Math.sin(delLatitude / 2);
-    var term2 = Math.sin(delLongitude / 2);
-
-    var delSigma = 2 * Math.asin(Math.sqrt(Math.pow(term1, 2) + Math.cos(firstLatitude) * Math.cos(secondLatitude) * Math.pow(term2, 2)));
-    var myDistance = EARTH_RADIUS * delSigma;
-
-    console.log("my calculation: " + myDistance);
-    
-    // use my value for now
-    return myDistance;*/
-    
+function calculateDistance1(firstCoordinate, secondCoordinate) {
     var distanceObject = {
         firstCoordinate: convertCoordinate(firstCoordinate),
         secondCoordinate: convertCoordinate(secondCoordinate)
     };
-    
+
     return request
-                            .post("http://localhost:9080/distance")
-                            .send(distanceObject)
-                            .set('Accept', 'application/json')
+            .post("http://localhost:9080/distance")
+            .send(distanceObject)
+            .set('Accept', 'application/json')
             .then((res) => JSON.parse(res.text))
-            .then((res) => res.distance);
+            .then((res) => res.distance)
+            .catch((error) => { console.log("error: " + error); return calculateDistanceInternal(firstCoordinate, secondCoordinate); });
 }
 
 function calculateDistance2(firstCoordinate, secondCoordinate) {
@@ -62,7 +65,7 @@ function calculateDistance2(firstCoordinate, secondCoordinate) {
 
     var distance = geodist(coord1, coord2);
     console.log("other calculation : " + distance);
-    
+
     return distance;
 }
 
@@ -81,11 +84,11 @@ export function updateCoordinates(firstCoordinate, secondCoordinate) {
         // up the rest of the processing while the calculation is being done, 
         // potentially via a service call.  (not in this example, but in principle 
         // it could).
-        var calculatorArray = [ 
+        var calculatorArray = [
             {
                 calculator: calculateDistance1,
                 event: ActionTypes.UPDATE_DISTANCE_1
-            }, 
+            },
             {
                 calculator: calculateDistance2,
                 event: ActionTypes.UPDATE_DISTANCE_2
@@ -93,9 +96,9 @@ export function updateCoordinates(firstCoordinate, secondCoordinate) {
         calculatorArray.map((calculatorDescriptor) => {
             console.log("initiating calculation for " + calculatorDescriptor.event);
             makeCalculationPromise(calculatorDescriptor.calculator, firstCoordinate, secondCoordinate)
-                .then((distance) => {
-                    dispatch({type: calculatorDescriptor.event, distance});
-                });
+                    .then((distance) => {
+                        dispatch({type: calculatorDescriptor.event, distance});
+                    });
         });
     };
 }
