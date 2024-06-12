c274484966f435780d4f2e84df1e9fe7d7b39cf8
AAR1069 <scottaaron@northwesternmutual.com>
2024-06-12 11:32:23 -0500

    modernize CoordinatesMap

--- src/js/components/CoordinatesMap.jsx
+++ src/js/components/CoordinatesMap.jsx
@@ -4,43 +4,32 @@
  * and open the template in the editor.
  */
 
-import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
 import CoordinateMarker from './CoordinateMarker';
 
-export default class CoordinatesMap extends Component {
-    constructor(props) {
-        super(props);
-    }
+const CoordinatesMap = (props) => {
+    return (
+        <div id="displayMap">
+            <GoogleMapReact
+                defaultCenter={props.center}
+                defaultZoom={props.zoom}
+		bootstrapURLKeys={{ key: "AIzaSyBXUyrNKCgNRfCDet9IMp-UVaPwfJ08QLE" }}
+            >
+                <CoordinateMarker
+                    lat={props.coordinates.firstCoordinate.lat}
+                    lng={props.coordinates.firstCoordinate.lng}
+                    text="1"
+                />
+                <CoordinateMarker
+                    lat={props.coordinates.secondCoordinate.lat}
+                    lng={props.coordinates.secondCoordinate.lng}
+                    text="2"
+                />
+            </GoogleMapReact>
+        </div>
+    );
+};
 
-    static defaultProps = {
-        center: {
-            lat: 59.95,
-            lng: 30.33
-        },
-        zoom: 1
-    };
+CoordinatesMap.defaultProps = {center: { lat: 59.95, lng: 30.33 }, zoom: 1};
 
-    render() {
-        return (
-                <div id="displayMap">
-                    <GoogleMapReact
-                        defaultCenter={this.props.center}
-                        defaultZoom={this.props.zoom}
-			bootstrapURLKeys={{ key: "AIzaSyBXUyrNKCgNRfCDet9IMp-UVaPwfJ08QLE" }}
-                        >
-                        <CoordinateMarker
-                            lat={this.props.coordinates.firstCoordinate.lat}
-                            lng={this.props.coordinates.firstCoordinate.lng}
-                            text="1"
-                            />
-                        <CoordinateMarker
-                            lat={this.props.coordinates.secondCoordinate.lat}
-                            lng={this.props.coordinates.secondCoordinate.lng}
-                            text="2"
-                            />
-                    </GoogleMapReact>
-                </div>
-                            );
-                }
-    }
+export default CoordinatesMap;
