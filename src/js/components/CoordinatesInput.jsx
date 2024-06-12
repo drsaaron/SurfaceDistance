78633c35f486aeb40f3fd1fc23ab4e5a078794c8
AAR1069 <scottaaron@northwesternmutual.com>
2024-06-12 11:23:40 -0500

    modernize CoordinateInput

--- src/js/components/CoordinatesInput.jsx
+++ src/js/components/CoordinatesInput.jsx
@@ -4,84 +4,70 @@
  * and open the template in the editor.
  */
 
-import React, {Component} from 'react';
-export default class CoordinatesInput extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstLatitude: this.props.coordinates.firstCoordinate.lat || 0,
-            firstLongitude: this.props.coordinates.firstCoordinate.lng || 0,
-            secondLatitude: this.props.coordinates.secondCoordinate.lat || 0,
-            secondLongitude: this.props.coordinates.secondCoordinate.lng || 0
-        }
+import {useState, useEffect} from 'react';
 
-        this.updateFirstLatitude = this.updateFirstLatitude.bind(this);
-        this.updateSecondLatitude = this.updateSecondLatitude.bind(this);
-        this.updateFirstLongitude = this.updateFirstLongitude.bind(this);
-        this.updateSecondLongitude = this.updateSecondLongitude.bind(this);
-        this.handleUpdateMap = this.handleUpdateMap.bind(this);
+const CoordinatesInput = (props) => {
+
+    const [firstLatitude, setFirstLatitude] = useState(props.coordinates.firstCoordinate.lat || 0);
+    const [firstLongitude, setFirstLongitude] = useState(props.coordinates.firstCoordinate.lng || 0);
+    const [secondLatitude, setSecondLatitude] = useState(props.coordinates.secondCoordinate.lat || 0);
+    const [secondLongitude, setSecondLongitude] = useState(props.coordinates.secondCoordinate.lng || 0);
+
+    const updateFirstLatitude = (event) => {
+	setFirstLatitude(event.target.value);
     }
 
-    updateFirstLatitude(event) {
-        this.setState({...this.state, firstLatitude: event.target.value});
+    const updateSecondLatitude = (event) => {
+	setSecondLatitude(event.target.value);
     }
 
-    updateSecondLatitude(event) {
-        this.setState({...this.state, secondLatitude: event.target.value});
+    const updateFirstLongitude = (event) => {
+        setFirstLongitude(event.target.value);
     }
 
-    updateFirstLongitude(event) {
-        this.setState({...this.state, firstLongitude: event.target.value});
+    const updateSecondLongitude = (event) => {
+        setSecondLongitude(event.target.value);
     }
 
-    updateSecondLongitude(event) {
-        this.setState({...this.state, secondLongitude: event.target.value});
-    }
-
-    componentDidMount() {
-        if (this.props.coordinates.hasFirstCoordinate && this.props.coordinates.hasSecondCoordinate) {
-            this.updateMap();
-        }
-    }
-    
-    updateMap() {
-        var firstCoordinate = { lat: this.state.firstLatitude, lng: this.state.firstLongitude };
-        var secondCoordinate = { lat: this.state.secondLatitude, lng: this.state.secondLongitude };
+    const updateMap = () => {
+        var firstCoordinate = { lat: firstLatitude, lng: firstLongitude };
+        var secondCoordinate = { lat: secondLatitude, lng: secondLongitude };
         
-        this.props.updateCoordinates(firstCoordinate, secondCoordinate);
+        props.updateCoordinates(firstCoordinate, secondCoordinate);
     }
     
-    handleUpdateMap(event) {
+    const handleUpdateMap = (event) => {
         event.preventDefault();
         
-        this.updateMap();
+        updateMap();
     }
-
-    render() {
-        return (
-                <div id="coordinateInput">
-                    <form onSubmit={this.handleUpdateMap} id="coordinateInputForm">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td><label htmlFor="firstLatitude">Latitude 1</label></td>
-                                    <td><input type="text" name="firstLatitude" value={this.state.firstLatitude} onChange={this.updateFirstLatitude} /></td>
-                                    <td><label htmlFor="firstLongitude">Longitude 1</label></td>
-                                    <td><input type="text" name="firstLongitude" value={this.state.firstLongitude} onChange={this.updateFirstLongitude} /></td>            
-                                </tr>
-                                <tr>
-                                    <td><label htmlFor="secondLatitude">Latitude 2</label></td>
-                                    <td><input type="text" name="secondLatitude" value={this.state.secondLatitude} onChange={this.updateSecondLatitude} /></td>
-                                    <td><label htmlFor="secondLongitude">Longitude 2</label></td>
-                                    <td><input type="text" name="secondLongitude" value={this.state.secondLongitude} onChange={this.updateSecondLongitude} /></td>            
-                                </tr>     
-                                <tr>
-                                    <td colSpan="4" className="alignRight"><input type="submit" value="Update" /></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </form>
-                </div>
-                );
-    }
+    
+    return (
+        <div id="coordinateInput">
+            <form onSubmit={handleUpdateMap} id="coordinateInputForm">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td><label htmlFor="firstLatitude">Latitude 1</label></td>
+                            <td><input type="text" name="firstLatitude" value={firstLatitude} onChange={updateFirstLatitude} /></td>
+                            <td><label htmlFor="firstLongitude">Longitude 1</label></td>
+                            <td><input type="text" name="firstLongitude" value={firstLongitude} onChange={updateFirstLongitude} /></td>            
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="secondLatitude">Latitude 2</label></td>
+                            <td><input type="text" name="secondLatitude" value={secondLatitude} onChange={updateSecondLatitude} /></td>
+                            <td><label htmlFor="secondLongitude">Longitude 2</label></td>
+                            <td><input type="text" name="secondLongitude" value={secondLongitude} onChange={updateSecondLongitude} /></td>            
+                        </tr>     
+                        <tr>
+                            <td colSpan="4" className="alignRight"><input type="submit" value="Update" /></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+    );
 }
+
+export default CoordinatesInput;
+
